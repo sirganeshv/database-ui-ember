620579550309a5afa8b837a80c181ea92cec3ec3
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-03-09 15:00:20 +0530

    Basic version of database-ui done
    Table name is inputted and corresponding table is displayed

---
+++ WEB-INF/src/DatabaseServlet.java
@@ -0,0 +1,79 @@
+import java.sql.*; 
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+import java.io.*;  
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+import org.json.*;
+ 
+public class DatabaseServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req,HttpServletResponse res)  
+	throws ServletException,IOException {
+		doProcess(req,res);
+	}
+
+	public void doPost(HttpServletRequest req,HttpServletResponse res)  
+	throws ServletException,IOException  {  
+		doProcess(req,res);
+	}
+	
+    public void doProcess(HttpServletRequest req,HttpServletResponse res)  
+	throws ServletException,IOException {  
+		PrintWriter pw=res.getWriter();
+        try{  
+			String table_name = req.getParameter("table_name");
+            Connection conn = getConnection();
+			DatabaseMetaData dbm = conn.getMetaData();
+			ResultSet tables = dbm.getTables(null, null, table_name, null);
+			if (tables.next()) {
+				getServletContext().log("Table exists");
+				Statement stmt = conn.createStatement(); 
+				ResultSet rs = stmt.executeQuery("select * from "+table_name);
+				getServletContext().log("The result of the query is "+rs);
+				ResultSetMetaData rsmd = rs.getMetaData();              
+				System.out.println("Total columns: "+rsmd.getColumnCount()); 
+				System.out.println();
+				JSONObject obj = new JSONObject();
+				JSONArray col = new JSONArray();
+				for(int i = 0;i < rsmd.getColumnCount();i++ ) {
+					System.out.print(rsmd.getColumnName(i+1)+"\t");
+					col.add(i, rsmd.getColumnName(i+1));
+				}
+				obj.put("col",col);
+				System.out.println();
+				int j = 0;
+				JSONArray row = new JSONArray();
+				while(rs.next()) {
+					JSONArray rowi = new JSONArray();
+					for(int i = 0 ;i < rsmd.getColumnCount();i++) {
+						rowi.add(rs.getString(i+1));
+						System.out.print(rs.getString(i+1)+"\t");
+					}
+					row.add(rowi);
+					j++;
+					System.out.println();
+				}
+				obj.put("row",row);
+				pw.println(obj);
+			}
+			else
+				pw.println("false");
+            conn.close();
+        }
+        catch(Exception e){ 
+            System.out.println(e);
+        }  
+		finally {
+			pw.close();
+		}
+    }
+    private static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/Banking";
+        return DriverManager.getConnection(url, "root", "");
+  }
+}  
--- database-ui/app/controllers/index.js
+++ database-ui/app/controllers/index.js
@@ -9,18 +9,36 @@ export default Controller.extend({
         url: "/get_table",
         type: "POST",
         data: {
-            "table_name" : this.get('table_name')
+          "table_name" : this.get('table_name')
         }
       }).then(function(resp){
-        /*var data = JSON.parse(resp);
-        that.set('name',data.name);
-        that.set('age',data.age);*/
-        var data  = resp;
-        /*if(data.trim() == 'false')
-          that.set('message',"Enter your credentials correctly");
-        else if(data.trim() == 'true'){
-          that.transitionToRoute('/home');
-        }*/
+          var data = JSON.parse(resp);
+          var table = document.getElementById("table");
+          var rowCount = table.rows.length;
+          for (var x=rowCount-1; x>=0; x--) {
+					  table.deleteRow(x);
+					}
+          if(resp.trim() == 'false') {
+            //alert("No table");
+            that.set('message','No such table exists');
+          }
+          else {
+            that.set('message','');
+            $('#tablediv').css("display","initial");
+            var header = table.createTHead();
+            var row = header.insertRow(0);
+            for(var i = 0; i < data.col.length;i++) {
+              var cell = row.insertCell(i);
+              cell.innerHTML = data.col[i];
+  					}
+            for(var i = 0; i < data.row.length;i++) {
+  						var row = table.insertRow(i+1);
+              for(var j =0 ;j < data.row[i].length;j++) {
+                var cell1 = row.insertCell(j);
+                cell1.innerHTML = data.row[i][j];
+              }
+            }
+          }
       }).catch(function(error){
         alert(JSON.stringify(error));
       });
