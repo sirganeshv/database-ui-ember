59f2a28f2b8d38789e4df3a0a938ea2963ed83c7
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-03-14 10:32:13 +0530

    Table display neatly Code refactored Component style refactored

--- database-ui/app/controllers/index.js
+++ database-ui/app/controllers/index.js
@@ -15,38 +15,24 @@ export default Controller.extend({
           data = JSON.parse(resp);
           var table = document.getElementById("table");
           var rowCount = table.rows.length;
-          for (var x=rowCount-1; x>=0; x--) {
+          for (var x=rowCount-1; x>0; x--) {
 					  table.deleteRow(x);
 					}
           if(resp.trim() == 'false') {
-            //alert("No table");
             that.set('message','No such table exists');
           }
           else {
             that.set('message','');
           }
-            /*$('#tablediv').css("display","initial");
-            var header = table.createTHead();
-            var row = header.insertRow(0);
-            for(var i = 0; i < data.col.length;i++) {
-              var cell = row.insertCell(i);
-              cell.innerHTML = data.col[i];
-  					}
-            for(var i = 0; i < data.row.length;i++) {
-  						var row = table.insertRow(i+1);
-              for(var j =0 ;j < data.row[i].length;j++) {
-                var cell1 = row.insertCell(j);
-                cell1.innerHTML = data.row[i][j];
-              }
-            }
-          }*/
+          that.send('setData');
+          that.send('getData');
       }).catch(function(error){
-        alert(JSON.stringify(error));
+        alert(error);
       });
     },
-  test() {
-    var tableData = data;
-    this.set('tableData',tableData);
+    setData() {
+      var tableData = data;
+      this.set('tableData',tableData);
+    }
   }
-}
 });
--- database-ui/app/routes/index.js
+++ database-ui/app/routes/index.js
@@ -2,26 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-      //return this.get('store').findAll('post');
-      /*return [{
-        title: 'Grand Old Mansion',
-        body: 'it is a good mansion',
-        rate: 4300
-      }, {
-        title: 'Urban Living',
-        body: 'Not bad',
-        rate: 4500
-      }, {
-        title: 'Downtown Charm',
-        body: 'Ok ok',
-        rate: 5000
-      }];*/
-    var data = this.controllerFor('index').get('tableData');
     return this.controllerFor('index').get('tableData');
   },
   actions : {
-    tesst() {
-      var data =this.controllerFor('index').get('tableData');
+    getData() {
       this.refresh();
     }
   }
